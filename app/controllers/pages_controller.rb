7f9128287c44cc8d8127d42a9491d4063af18410
Pierre Prevoteau <pierre.prevoteau@askida.fr>
2016-02-10 22:38:13 +0100

    Yep Yep Yep !

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,15 @@ class PagesController < ApplicationController
   end
 
   def overview
-    @media = Medium.all.order(id: :desc)
+    @pending = Medium.all.where(state: ["0", "1"]).order(id: :desc)
+
+    @workers = Resque.workers
+
+    workings = Resque.working
+    jobs = workings.collect {|w| w.job }
+    worker_jobs = workings.zip(jobs)
+    @worker_jobs = worker_jobs.reject { |w, j| w.idle? }
+
+    @completed = Medium.all.where(state: ["2", "3"]).order(id: :desc)
   end
 end
