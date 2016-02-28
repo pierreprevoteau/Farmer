6861673d7078c41c8448ec7a3cb157051f3d41ea
Pierre Prevoteau <pierre.prevoteau@askida.fr>
2016-02-28 20:44:27 +0100

    Work on Purge and Layout

--- app/jobs/purge_job.rb
+++ app/jobs/purge_job.rb
@@ -5,8 +5,12 @@ class PurgeJob
   def self.perform
     puts "**** Performing purge ****"
 
-      # TODO #
-      sleep 30
+    @media = Medium.all.where(state: "copied_to_out_directory").where("updated_at <= :date", date: Time.now.yesterday.beginning_of_day)
+
+    @media.each do |medium|
+      ApplicationController.set_state_to_ready_for_purge(medium.id)
+      
+    end
 
   end
 end
